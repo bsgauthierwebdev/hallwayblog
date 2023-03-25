33493e18cf9e245a2738474449498c752dec82bc
Brent Gauthier <brentsgauthier@gmail.com>
2023-03-25 00:11:01 -0400

    Build & Style Home Page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -49,13 +49,13 @@ const Home = () => {
   return (
     <div className = 'home'>
       <div className="posts">
-        {posts.map(post=>(
+        {posts.map(post => (
           <div className="post" key = {post.id}>
             <div className="img">
-              <img src={post.img} alt="" />
+              <img src = {post.img} alt="post image" />
             </div>
-            <div className="description">
-              <Link className = 'link' to = {`'/posts/${post.id}`}>
+            <div className="content">
+              <Link className = 'link' to = {`/posts/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
               <p>{post.description}</p>
