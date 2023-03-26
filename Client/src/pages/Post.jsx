37a8071474c906203abe5a5d4b04a68a28a12f40
Brent Gauthier <brentsgauthier@gmail.com>
2023-03-26 13:05:41 -0400

    Start building & styling single post page

--- Client/src/pages/Post.jsx
+++ Client/src/pages/Post.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Post = () => {
   return (
-    <div>Post</div>
+    <div className = 'post'>
+      <div className="content">c</div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
