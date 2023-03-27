37a27cc80435339d675b8c377bdfd7565579e81d
Brent Gauthier <brentsgauthier@gmail.com>
2023-03-27 11:37:43 -0400

    Added db seeding

---
+++ API/seeds/seed.users_and_posts.sql
@@ -0,0 +1,40 @@
+TRUNCATE users, posts RESTART IDENTITY CASCADE;
+
+INSERT INTO users (username, email, pw, img)
+VALUES
+    ('testuser', 'test@test.com', 'testpassword', 'testimageurl'),
+    ('testuser2', 'test2@test.com', 'testpassword2', 'testimageurl2');
+
+INSERT INTO posts (title, descr, content, img, userid)
+VALUES  
+    (
+        'Test Post 1',
+        'This is the first test post',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Test Image 1',
+        1
+    ),
+
+    (
+        'Test Post 2',
+        'This is the second test post',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Test Image 2',
+        2
+    ),
+
+    (
+        'Test Post 3',
+        'This is the third test post',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Test Image 3',
+        1
+    ),
+
+    (
+        'Test Post 4',
+        'This is the fourth test post',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Test Image 4',
+        2
+    );
