891576a860f7af546d39cea06703fe043df3ae09
Brent Gauthier <brentsgauthier@gmail.com>
2023-03-27 11:37:24 -0400

    Changed password column in users table

--- API/migrations/001.do.create_users.sql
+++ API/migrations/001.do.create_users.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(45) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(45) NOT NULL,
+    pw VARCHAR(45) NOT NULL,
     img VARCHAR(1000)
 );
